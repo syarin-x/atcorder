135453695e892ee015d2ee55242de408cf7bc46f
mutax <magicspell.max.mix.mutax@gmail.com>
2020-06-01 21:29:07 +0900

    2020/06/01 submit

---
+++ submit/AtCoder Beginner Contest/ABC111/b.cpp
@@ -0,0 +1,91 @@
+// include
+// ------------------------------------------------
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+// func
+// ------------------------------------------------
+int CalcSumOfDigit(int n);      // 各桁の和を計算する。
+string upper(string str);       // 英字を大文字に変換する。
+string lower(string str);       // 英字を小文字に変換する。
+
+// define
+// ------------------------------------------------
+#define all(a)        (a).begin(),(a).end()
+#define rall(a)       (a).rbegin(),(a).rend()
+#define pb            push_back
+#define sz(a)         int((a).size())
+#define rep(i,n)      for(int(i)=0;(i)<(n);(i)++)
+#define repe(i,n)     for(int(i)=0;(i)<=(n);(i)++)
+#define vsort(v)      sort((v).begin(),(v).end())
+#define rvsort(v)     sort(rall((v)))
+#define vi            vector<int>
+#define GCD(a,b)      __gcd((a),(b))
+#define LCM(a,b)      (a)/GCD((a),(b))*(b)
+#define kiriage(a,b)  ((a)+(b)-1)/(b)
+
+const int INF = 1e9;
+
+// code
+// ------------------------------------------------
+int main() {
+
+  int x; cin >> x;
+  int ans = 0;
+
+  while(1)
+  {
+    string s;
+    s = to_string(x);
+
+    if(s[0] == s[1] && s[1] == s[2])
+    {
+      break;
+    }
+    x++;
+  }
+
+  cout << x << endl;
+
+  return 0;
+}
+// funcの実体
+// ------------------------------------------------
+int CalcSumOfDigit(int n)
+{
+  int s = 0;
+  while(n)
+  {
+    s += n % 10;
+    n = n / 10;
+  }
+  return s;
+}
+
+string upper(string str)
+{
+  for(auto itr = str.begin();itr != str.end() ; itr++)
+  {
+    if(97 <= *itr && *itr <= 122)
+    {
+      *itr = *itr - 32;
+    }
+  }
+  return str;
+}
+
+string lower(string str)
+{
+  for(auto itr = str.begin();itr != str.end() ; itr++)
+  {
+    if(65 <= *itr && *itr <= 90)
+    {
+      *itr = *itr + 32;
+    }
+  }
+  return str;
+}
